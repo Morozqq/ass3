931eff7d524d72347c6dfcdd59de23e2a538ec7e
Morozqq <122681980+Morozqq@users.noreply.github.com>
2023-05-01 21:51:20 +0600

    Update MyHashTable.java

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -51,15 +51,61 @@ public class MyHashTable<K, V> {
         size++;
     }
 
-    public V get(K key) {
+  public V get(K key) {
+        int index = hash(key);
+        HashNode<K,V> node = chainArray[index];
+        while (node!= null){
+            if (node.key.equals(key)){
+                return node.value;
+            }
+            node=node.next;
+        }
+        return null;
     }
 
     public V remove(K key) {
+        int index = hash(key);
+        HashNode<K,V> node = chainArray[index];
+        HashNode<K,V> prev = null;
+        while (node != null) {
+            if (node.key.equals(key)) {
+                if (prev == null) {
+                    chainArray[index] = node.next;
+                } else {
+                    prev.next = node.next;
+                }
+                size--;
+                return node.value;
+            }
+            prev = node;
+            node = node.next;
+        }
+        return null;
     }
 
     public boolean contains(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K,V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
     }
 
     public K getKey(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K,V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null;
     }
 }
